f8be216caf7c8fbb3ca7f74b8a09105b14a8aef8
amjedidiah <imunacode@gmail.com>
2020-08-28 13:24:18 +0100

    feat: Routing and prop-types

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 // Module imports
 import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+
+// Component imports
+import Home from 'components/home';
 
 // Asset imports
 import 'assets/css/app.css';
 
 export default class App extends React.Component {
-  render = () => <>Learn React</>
+  render = () => (
+    <Router>
+      <Route path="/" exact component={Home} />
+    </Router>
+  );
 }
