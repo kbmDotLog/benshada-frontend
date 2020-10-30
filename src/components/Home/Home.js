f018c9418952db9ec7507176486a2f227a00f643
amjedidiah <imunacode@gmail.com>
2020-10-30 16:45:34 +0100

    feat: implemented Home page redesign

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,24 +1,29 @@
 /* eslint-disable no-underscore-dangle */
-// Module imports
-import React from 'react';
+/** Module imports */
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { uniqBy, orderBy } from 'lodash';
-
-// Component imports
-import { randNum } from 'assets/js/prototypes.js';
-import categories from 'assets/js/categories.js';
 import { Link } from 'react-router-dom';
-import Jumbotron from './Jumbotron/Jumbotron.js';
-import ProductList from '../ProductList/ProductList.js';
-import HrFr from '../HrFr/HrFr.js';
-import Lookbook from './LookBook/LookBook.js';
+import { connect } from 'react-redux';
 
-// Asset imports
-import '../../assets/css/home.min.css';
-import HowItWorks from './HowItWorks/HowItWorks.js';
+/** Component imports */
+import Jumbotron from 'components/Home/Jumbotron/Jumbotron';
+import ProductList from 'components/ProductList/ProductList';
+import HrFr from 'components/HrFr/HrFr';
+import LookBook from 'components/Home/LookBook/LookBook';
+import HowItWorks from 'components/Home/HowItWorks/HowItWorks';
 
-// Start Component
-class Home extends React.Component {
+/** Asset imports */
+import 'assets/css/home.min.css';
+import { randNum } from 'assets/js/prototypes';
+import categories from 'assets/js/categories';
+
+/**
+ * Displays Home
+ * @constructor
+ */
+class Home extends Component {
+  /** Component propTypes */
   static propTypes = {
     isSignedIn: PropTypes.bool,
     orders: PropTypes.array,
@@ -26,8 +31,19 @@ class Home extends React.Component {
     stores: PropTypes.array
   };
 
-  getSales = ({ _id }) => this.props.orders.filter(({ product, status }) => product === _id && status === 'paid').length;
+  /**
+   * @param {Obj} product
+   * @return {number} No of sales for a particular product
+   */
+  getSales = ({ _id }) => this.props.orders.filter(
+    ({ product, status }) => product === _id && status === 'paid'
+  ).length;
 
+  /**
+   * @param {[Obj]} stores
+   * @param {[Obj]} products
+   * @return {[Obj]} Products for store lookbook
+   */
   getStoreLookBook = (stores, products) => {
     // Store needs to have at least 2 products
     const storeWithProducts = stores.filter((item) => item.products.length > 1);
@@ -62,17 +78,25 @@ class Home extends React.Component {
       title: 'Featured Shops',
       bgSmall: product1 && product1.image[0],
       bgBig: product2 && product2.image[0],
-      to: `/stores/${storeWithProductsAndSales[0] && storeWithProductsAndSales[0]._id}`
+      to: `/stores/${
+        storeWithProductsAndSales[0] && storeWithProductsAndSales[0]._id
+      }`
     };
   };
 
+  /**
+   * @param {[Obj]} products
+   * @return {[Obj]} Products for gender lookbook
+   */
   getGenderLookBook = (products) => {
     const genderTitles = [
       { title: 'Men', gender: 'male' },
       { title: 'Women', gender: 'female' }
     ];
     const randomGender = genderTitles[randNum(1)];
-    const productsByGender = products.filter(({ gender }) => gender === randomGender.gender);
+    const productsByGender = products.filter(
+      ({ gender }) => gender === randomGender.gender
+    );
 
     const randBg = () => {
       const product = productsByGender[randNum(productsByGender.length - 1)];
@@ -87,6 +111,10 @@ class Home extends React.Component {
     };
   };
 
+  /**
+   * @param {[Obj]} products
+   * @return {[Obj]} Products for discount lookbook
+   */
   getDiscountLookBook = (products) => {
     const discountedProductsWithSales = products.filter(
       ({ discountPercentage }) => discountPercentage > 0
@@ -108,24 +136,34 @@ class Home extends React.Component {
 
     return {
       title: 'Discounted Products',
-      bgSmall: discountedProductsOrdered[1] && discountedProductsOrdered[1].image,
+      bgSmall:
+        discountedProductsOrdered[1] && discountedProductsOrdered[1].image,
       bgBig: discountedProductsOrdered[0] && discountedProductsOrdered[0].image,
       to: '/catalog?a=p&discount=1',
       list
     };
   };
 
+  /**
+   * Renders category divs
+   * @param {[Obj]} cats
+   * @param {[Obj]} products
+   */
   renderCategories = (cats, products) => cats.map(({ name }, i) => {
-    const product = products.filter((prod) => prod.category === name)[0];
+    const product = products.find((prod) => prod.category === name);
     const image = ((product && product.image) || [])[0];
 
     return (
         <article
           key={`home-category-${i}`}
-          className={`shadow-sm category bg-secondary-gradient ${i > 1 && 'd-none d-xl-block'}`}
+          className={`shadow-sm category bg-secondary-gradient ${
+            i > 1 && 'd-none d-xl-block'
+          }`}
         >
           <div
-            style={{ background: image && `url(${image}) no-repeat bottom left/cover` }}
+            style={{
+              background: image && `url(${image}) no-repeat bottom left/cover`
+            }}
             className="category-image bg-secondary-gradient"
           ></div>
           <div className="v-parent bg-secondary-gradient">
@@ -140,44 +178,87 @@ class Home extends React.Component {
     );
   });
 
+  /**
+   * Displays Home UI
+   * @return {Obj} The UI DOM object
+   */
   render = () => {
     const { products, stores, isSignedIn } = this.props;
     const productsPlusSales = products.map((item) => ({
       ...item,
       sales: this.getSales(item)
     }));
-    const productsWithImages = productsPlusSales.filter(({ image }) => (image || []).length > 0);
+    const productsWithImages = productsPlusSales.filter(
+      ({ image }) => (image || []).length > 0
+    );
     const productsBySales = orderBy(productsWithImages, ['sales'], ['desc']) || [];
 
     return (
       <HrFr>
-        <Jumbotron isSignedIn={isSignedIn} />
-        <section className="productShowcase mt-4 section">
-          <h2 className="d-none">Recent Products</h2>
-          <ProductList isFlex={true} products={products} count={12} title="Recently Added" />
-        </section>
+        <Jumbotron
+          isSignedIn={isSignedIn}
+          title="2020"
+          subTitle={
+            <span>
+              Latest
+              <br />
+              Collection
+            </span>
+          }
+        />
+        <ProductList
+          isFlex={true}
+          products={products}
+          count={12}
+          title="Recently Added"
+        />
 
-        <Lookbook data={this.getStoreLookBook(stores, productsBySales)} variant="right" />
+        <LookBook
+          data={this.getStoreLookBook(stores, productsBySales)}
+          variant="right"
+        />
 
-        <section className="productShowcase mt-4 section">
-          <h2 className="d-none">Products By Sales</h2>
-          <ProductList isFlex={true} products={productsBySales} count={12} title="Recently Added" />
-        </section>
+        <ProductList
+          isFlex={true}
+          products={productsBySales}
+          count={12}
+          title="Products By Sales"
+        />
 
-        <Lookbook data={this.getGenderLookBook(productsBySales)} flipped={true} variant="left" />
+        <LookBook
+          data={this.getGenderLookBook(productsBySales)}
+          flipped={true}
+          variant="left"
+        />
 
         <section className="section" id="categories">
           <h2 className="d-none">categories</h2>
           {this.renderCategories(categories, productsBySales)}
         </section>
 
-        <Lookbook data={this.getDiscountLookBook(productsBySales)} variant="right" />
+        <LookBook
+          data={this.getDiscountLookBook(productsBySales)}
+          variant="right"
+        />
         <HowItWorks />
       </HrFr>
     );
   };
 }
-// End Component
 
-// Export component as React-functional-Component
-export default Home;
+/**
+ * Component mapStateToProps
+ * @param {Obj} state
+ * @return {Obj} Extra component props
+ */
+const mapStateToProps = ({
+  auth, order, product, store
+}) => ({
+  isSignedIn: auth.isSigned,
+  orders: order.all,
+  products: product.all,
+  stores: store.all
+});
+
+/** Export component */
+export default connect(mapStateToProps)(Home);
